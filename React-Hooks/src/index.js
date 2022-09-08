a95e88f93edfba1514d267eae7a9bdbde5561a58
AhmedShaykh <ahmed.shaykhwork@gmail.com>
2022-09-08 16:27:43 +0500

    Git Init

--- React-Hooks/src/index.js
+++ React-Hooks/src/index.js
@@ -11,7 +11,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
     <Routes>
-      <Route path='/' element={<App />} />
+      <Route exact path='/' element={<App />} />
       <Route path='/home' element={<HeroPage />} />
       <Route path='/data' element={<DataPage />} />
       <Route path='/hooks' element={<HooksPage />} />
